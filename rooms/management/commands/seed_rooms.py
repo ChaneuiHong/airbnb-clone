a3a415612ce011506b8550cbf93a910e4ca78225
Chani <hce714@gmail.com>
2022-02-07 22:07:28 +0900

    commit seeder generation code for Phto,Amenities,Facilities,HouseRules

--- rooms/management/commands/seed_rooms.py
+++ rooms/management/commands/seed_rooms.py
@@ -1,6 +1,7 @@
 import random
 from django.core.management.base import BaseCommand
 from django_seed import Seed
+from django.contrib.admin.utils import flatten
 from users import models as users_models
 from rooms import models as rooms_models
 
@@ -18,11 +19,12 @@ class Command(BaseCommand):
         number = options.get("number")
         seeder = Seed.seeder()
 
+        # room model 내 class 들
         all_users = users_models.Users.objects.all()
         room_types = rooms_models.RoomType.objects.all()
-
-        print(all_users)
-        print(room_types)
+        amenities = rooms_models.Amenity.objects.all()
+        facilities = rooms_models.Facility.objects.all()
+        rules = rooms_models.HouseRule.objects.all()
 
         seeder.add_entity(
             rooms_models.Room,
@@ -39,6 +41,34 @@ class Command(BaseCommand):
                 "guests": lambda x: random.randint(1, 20),
             },
         )
-        seeder.execute()
+        # seeder.execute의 반환값은 해당 데이터의 sequence(pk로 사용 가능)
+        created_photos = seeder.execute()
+        created_clean = flatten(created_photos.values())
+
+        # Room 모델 안에 photo를 seeding 하는 작업
+        # 위에서 생성된 Room을 순차적으로 실행
+        for pk in created_clean:
+            room = rooms_models.Room.objects.get(pk=pk)
+            # photo의 개수를 랜덤으로 생성함
+            for _ in range(3, random.randint(1, 10)):
+                rooms_models.Photo.objects.create(
+                    caption=seeder.faker.sentence(),
+                    files=f"room_photos/{random.randint(1,31)}.webp",
+                    room=room,
+                )
+            # Many To Many field에 seeding 하는 방법
+            for a in amenities:
+                magic_number = random.randint(1, 10)
+                if magic_number % 2 == 0:
+                    room.amenities.add(a)
+            for f in facilities:
+                magic_number = random.randint(1, 10)
+                if magic_number % 2 == 0:
+                    room.facilities.add(f)
+            for r in rules:
+
+                magic_number = random.randint(1, 10)
+                if magic_number % 2 == 0:
+                    room.house_rules.add(r)
 
         self.stdout.write(self.style.SUCCESS(f"{number} Rooms created!"))
